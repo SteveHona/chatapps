039c7e76e77c03b8f97220d51ec8a774a418dde5
Zigo Stephano <zigostephano@yahoo.com>
2021-11-08 15:17:09 +0700

    Finish splash screen

--- lib/app/utils/error_screen.dart
+++ lib/app/utils/error_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class LoadingPage extends StatelessWidget {
+class ErrorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Chat Apps",
       home: Scaffold(
-        body: Center(child: CircularProgressIndicator()),
+        body: Center(child: Text("Terjadi kesalahan")),
       ),
     );
   }
